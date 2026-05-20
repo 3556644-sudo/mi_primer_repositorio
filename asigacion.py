c75d5c3a17090ef164a0d783510b1a7b8a64e6ec
3556644-sudo <3556644@clases.edu.sv>
2026-05-20 15:12:17 -0600

    Add files via upload

---
+++ asigacion.py
@@ -0,0 +1,17 @@
+
+x,y,z = "sandia", "piña", "pepino"
+print(x)
+print(y)
+print(z)
+print("")
+x=y=z = "melocoton"
+print(x)
+print(y)
+print(z)
+print("")
+fruta = ["sandia", "piña", "pepino", "naranja"]
+x,y,z, a = fruta
+print(x)
+print(y)
+print(z)
+print(a)
